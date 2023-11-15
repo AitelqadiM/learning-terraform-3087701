7d01d950b1d83364e60d74595553abc373ebc3a4
Mohammed Ait El Qadi <92603852+AitelqadiM@users.noreply.github.com>
2023-11-15 10:09:55 +0100

    EC2 Docker

--- main.tf
+++ main.tf
@@ -29,6 +29,7 @@ resource "aws_instance" "web" {
               sudo apt-get install -y docker.io
               sudo systemctl enable docker
               sudo systemctl start docker
+              sudo docker run -d -p 80:80 --name hello-world-app nginx:alpine
               EOF
 }
 
